51f1b1e103067be28a55eafc00f81ad5b408882d
jasenpau <jasenasp@gmail.com>
2026-02-21 17:41:09 +0200

    Add button and text element

--- app/src/main/java/com/jasenas/fintechapp/MainActivity.kt
+++ app/src/main/java/com/jasenas/fintechapp/MainActivity.kt
@@ -4,11 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.jasenas.fintechapp.ui.theme.FinTechAppTheme
@@ -20,8 +28,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             FinTechAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MainView(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +38,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainView(modifier: Modifier = Modifier) {
+    var greetingText by remember { mutableStateOf("") }
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = greetingText)
+        Button(onClick = { greetingText = "hello" }) {
+            Text("Say hello")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainViewPreview() {
     FinTechAppTheme {
-        Greeting("Android")
+        MainView()
     }
 }
