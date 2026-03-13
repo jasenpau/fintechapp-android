c0abebeea075919e396a305baf48fc7b2a411ec0
jasenpau <jasenasp@gmail.com>
2026-03-13 19:49:44 +0200

    Add button to change text color

--- app/src/main/java/com/jasenas/fintechapp/MainActivity.kt
+++ app/src/main/java/com/jasenas/fintechapp/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,6 +19,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.jasenas.fintechapp.ui.theme.FinTechAppTheme
 
@@ -40,14 +42,24 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainView(modifier: Modifier = Modifier) {
     var greetingText by remember { mutableStateOf("") }
+    var textColor by remember { mutableStateOf(Color.Black) }
+    val primaryColor = MaterialTheme.colorScheme.primary
+
     Column(
         modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = greetingText)
+        Text(text = greetingText, color = textColor)
         Button(onClick = { greetingText = "hello" }) {
             Text("Say hello")
+        }
+        Button(onClick = {
+            textColor = if (textColor == primaryColor)
+                Color.Black
+            else primaryColor
+        }) {
+            Text("Change text color")
         }
     }
 }
